c558916c4fb12de6c2d477c1330021bd437faf56
Anderson Hiroshi Hamamoto <anderson.hamamoto@gmail.com>
2014-11-20 09:41:50 -0200

    read-data and cast-to-int added to utils

--- src/dsnsf_ga/core.clj
+++ src/dsnsf_ga/core.clj
@@ -1,11 +1,14 @@
 (ns dsnsf-ga.core
-  (:gen-class))
+  (:require [clojure.java.io :as io]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
 
-
+(defn test
+  []
+  (average (cast-to-int (read-data "/Users/manoweng/Documents/Code/github/dsnsf-ga/data/data.txt"))))
 
 (load "utils")
+
--- src/dsnsf_ga/utils.clj
+++ src/dsnsf_ga/utils.clj
@@ -9,3 +9,14 @@
   "returns random float in range"
   [inf sup]
   (+ inf (rand (- sup inf))))
+
+(defn read-data
+  "returns a list of strings, each element is the content of a line"
+  [file]
+  (with-open [rdr (io/reader file)]
+    (doall (line-seq rdr))))
+
+(defn cast-to-int
+  "casts all elements in list to interger"
+  [coll]
+  (map #(Integer/parseInt %) coll))
